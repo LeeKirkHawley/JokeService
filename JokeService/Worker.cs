2d2e4c4275e7aa5a23ba6a5c521f41b8a1d2b368
Lee Kirk Hawley <leekirkhawley@gmail.com>
2021-02-17 17:40:42 -0700

    calling web api, parsing result json

--- JokeService/Worker.cs
+++ JokeService/Worker.cs
@@ -5,6 +5,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using JokeService.Models;
+using Newtonsoft;
+using Newtonsoft.Json;
+
 
 namespace JokeService {
     public class Worker : BackgroundService {
@@ -16,7 +22,18 @@ namespace JokeService {
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
             while (!stoppingToken.IsCancellationRequested) {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                var client = new RestClient("https://api.ratesapi.io/api");
+                //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+                var request = new RestRequest("latest", DataFormat.Json);
+
+                var response = client.Get(request);
+                if(response.IsSuccessful == true) {
+                    RatesModel rates = JsonConvert.DeserializeObject<RatesModel>(response.Content);
+
+                }
+
                 await Task.Delay(1000, stoppingToken);
             }
         }
